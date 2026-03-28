b0c42e29c333ac65bed9bcb7f26e3471142b2e3c
PRUXIN <admin@pruxin.com>
2026-03-28 11:36:52 +0000

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,9 +1,5 @@
 import { ImageResponse } from '@vercel/og';
 
-export const config = {
-  runtime: 'edge',
-};
-
 export default async function handler(req) {
   const { searchParams } = new URL(req.url);
 
